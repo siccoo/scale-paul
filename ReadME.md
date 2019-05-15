06bb1a21d2c968bf3f6b1a94595714928b534294
Michael Chilaka <chilakamichael001@gmail.com>
2019-05-15 17:48:23 +0100

    script.js, ReadME.md

--- ReadME.md
+++ ReadME.md
@@ -1,3 +1,5 @@
-## What is Class and constructors?
+## What is Class and constructors function?
 
 class is just a pretty way of writing function constructors
+
+A Constructor function is the function that actually constructs our objects or create them
--- script.js
+++ script.js
@@ -29,3 +29,66 @@ fcmb.credit(3000);
 fcmb.balance();
 fcmb.withdraw(2800);
 fcmb.balance();
+
+// ==================================================================================
+
+// the new keyword
+// - creates a new empty object {}
+// - sets the value of this to be the new empty object
+// - calls the constructor method
+
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+}
+
+let userOne = new User('David Kel', 'daviddev@gmail.com');
+let userTwo = new User('Jerry Maed', 'maeddev@gmail.com');
+
+console.log(userOne);
+console.log(userTwo);
+
+/*
+-------ES6----
+
+write me a function called person 
+give it the parameter name 
+give it the parameter age 
+make the age have a default value of 26
+*/
+
+function Person(name, age=26){
+ this.name = name;
+ this.age = age;
+}
+
+let moya = new Person("moya", 37);
+let michael = new Person("michael");
+let tony = new Person();
+
+console.log("--------function constructtor -------")
+console.log(moya);
+console.log(michael);
+console.log(tony);
+
+
+//-------------------------------------------
+//function constructtor rewritten as a class
+
+class P{
+  constructor(name, age=26){
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const mo = new P("moya", 37);
+let mi = new P("michael");
+let to = new P();
+
+console.log("--------function constructtor rewritten as a class-------")
+console.log(mo);
+console.log(mi);
+console.log(to);
